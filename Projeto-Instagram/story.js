07334b16cf4306cc69a3ef977c6f9db985607c54
Nayara Melenchon <melenchonnayara@gmail.com>
2024-12-03 13:29:25 -0300

    Add files via upload

---
+++ Projeto-Instagram/story.js
@@ -0,0 +1,59 @@
+var crl1 = document.getElementById('MyCrl1').getContext('2d');
+
+crl1.beginPath();
+crl1.arc (60, 100, 40, 0, 2 *Math.PI);
+crl1.fillStyle = "#B0C4DE";
+crl1.fill();
+crl1.linewidth = 5;
+crl1.strokeStyle = '#dbdbdb';
+crl1.stroke();
+
+var crl2 = document.getElementById('MyCrl2').getContext('2d');
+
+crl2.beginPath();
+crl2.arc (60, 100, 40, 0, 2 *Math.PI);
+crl2.fillStyle = "#B0C4DE";
+crl2.fill();
+crl2.linewidth = 5;
+crl2.strokeStyle = '#dbdbdb';
+crl2.stroke();
+
+var crl3 = document.getElementById('MyCrl3').getContext('2d');
+
+crl3.beginPath();
+crl3.arc (60, 100, 40, 0, 2 *Math.PI);
+crl3.fillStyle = "#B0C4DE";
+crl3.fill();
+crl3.linewidth = 5;
+crl3.strokeStyle = '#dbdbdb';
+crl3.stroke();
+
+var crl4 = document.getElementById('MyCrl4').getContext('2d');
+
+crl4.beginPath();
+crl4.arc (60, 100, 40, 0, 2 *Math.PI);
+crl4.fillStyle = "#B0C4DE";
+crl4.fill();
+crl4.linewidth = 5;
+crl4.strokeStyle = '#dbdbdb';
+crl4.stroke();
+
+var crl5 = document.getElementById('MyCrl5').getContext('2d');
+
+crl5.beginPath();
+crl5.arc (60, 100, 40, 0, 2 *Math.PI);
+crl5.fillStyle = "#B0C4DE";
+crl5.fill();
+crl5.linewidth = 5;
+crl5.strokeStyle = '#dbdbdb';
+crl5.stroke();
+
+var crl6 = document.getElementById('MyCrl6').getContext('2d');
+
+crl6.beginPath();
+crl6.arc (60, 100, 40, 0, 2 *Math.PI);
+crl6.fillStyle = "#B0C4DE";
+crl6.fill();
+crl6.linewidth = 5;
+crl6.strokeStyle = '#dbdbdb';
+crl6.stroke();
